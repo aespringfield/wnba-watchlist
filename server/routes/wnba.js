bf9058d5c35250f0e95c3aa5ee251217b6415871
Anna Springfield <anna.e.springfield@gmail.com>
2018-08-10 17:12:18 -0500

    Add routes for today & tomorrow, allow user to set date range & point spread in request, and clean up date

--- server/routes/wnba.js
+++ server/routes/wnba.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const moment = require('moment');
 
 const withinRange = (game, range) => {
-    const gameDate = new Date(game.scheduled);
-    const today = new Date();
-    const minDate = new Date().setDate(new Date().getDate() - range);
-    return gameDate < today && gameDate > minDate;
+    const gameDate = moment(game.scheduled);
+    const today = moment();
+    const minDate = moment().subtract(range, 'days');
+    return gameDate < today.endOf('day') && gameDate > minDate.startOf('day');
 }
 
 const withinPointSpread = (game, pointSpread) => {
@@ -21,11 +22,20 @@ const getGoodGames = (games, range, pointSpread) => {
 
 // This function is not needed for the main functionality we're going for.
 // It just returns games that are happening today.
-const gamesToday = (games) => {
+const getGamesToday = (games) => {
     return games.filter((game) => {
         const gameDate = new Date(game.scheduled);
         const today = new Date();
         return gameDate.getDate() === today.getDate() && gameDate.getMonth() === today.getMonth();
+    })
+}
+
+const getGamesTomorrow = (games) => {
+    return games.filter((game) => {
+        const gameDate = moment(game.scheduled);
+        const today = moment()
+        const tomorrow = moment().add(1, 'days')
+        return gameDate > today.endOf('day') && gameDate <= tomorrow.endOf('day')
     })
 }
 
@@ -35,7 +45,7 @@ const stripOutScore = (games) => {
         return {
             id: id,
             status: status,
-            scheduled: scheduled,
+            scheduled: moment(scheduled).format('MMMM Do YYYY, h:mm a'),
             reference: reference,
             home: game.home,
             away: game.away
@@ -43,15 +53,42 @@ const stripOutScore = (games) => {
     })
 }
 
-router.get('/', (req, res) => {
+router.get('/:range/:pointSpread', (req, res) => {
+    request(`https://api.sportradar.us/wnba/trial/v4/en/games/2018/REG/schedule.json?api_key=${process.env.API_KEY}`, (error, response, body) => {
+        if (error) {
+            console.log('error:', error);
+        }
+
+        const range = parseInt(req.params.range);
+        const pointSpread = parseInt(req.params.pointSpread);
+
+        const games = JSON.parse(body).games;
+        const goodGames = getGoodGames(games, range, pointSpread);
+        res.send(stripOutScore(goodGames));
+    });
+})
+
+router.get('/today', (req, res) => {
     request(`https://api.sportradar.us/wnba/trial/v4/en/games/2018/REG/schedule.json?api_key=${process.env.API_KEY}`, (error, response, body) => {
         if (error) {
             console.log('error:', error);
         }
 
         const games = JSON.parse(body).games;
-        const goodGames = getGoodGames(games, 7, 6)
-        res.send(stripOutScore(goodGames));
+        const gamesToday = getGamesToday(games);
+        res.send(stripOutScore(gamesToday));
+    });
+})
+
+router.get('/tomorrow', (req, res) => {
+    request(`https://api.sportradar.us/wnba/trial/v4/en/games/2018/REG/schedule.json?api_key=${process.env.API_KEY}`, (error, response, body) => {
+        if (error) {
+            console.log('error:', error);
+        }
+
+        const games = JSON.parse(body).games;
+        const gamesTomorrow = getGamesTomorrow(games);
+        res.send(stripOutScore(gamesTomorrow));
     });
 })
 
